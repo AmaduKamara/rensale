8a7942e56b5fe0927fedc95a9bfb064761960cde
Amkam <amaduskamara@gmail.com>
2022-11-08 12:10:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 
 const App = () => {
-  const [active, setActive] = useState("home");
   return (
     <>
-      <Header active={active} setActive={setActive} />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
