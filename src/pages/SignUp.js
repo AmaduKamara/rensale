bcea13d9aa6d416d1d151c1e5f8f3c9eb40e5a10
Amkam <amaduskamara@gmail.com>
2022-11-08 14:44:00 +0000

    Update SignUp.js

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { FcGoogle } from "react-icons/fc";
+
 import { AiFillEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import OAuth from "../components/OAuth";
 
 const signup = "./images/auth/signup.png";
 
@@ -114,13 +115,7 @@ const SignUp = () => {
               <div className="my-3 flex items-center  before:border-t before:flex-1  before:border-gray-300 after:border-t after:flex-1  after:border-gray-300">
                 <p className="text-center font-semibold mx-4">OR</p>
               </div>
-              <div className="flex items-center justify-center mt-2">
-                <div className="h-12 w-12 rounded-full shadow-xl flex items-center justify-center border border-gray-100">
-                  <Link to="/">
-                    <FcGoogle size={30} />
-                  </Link>
-                </div>
-              </div>
+              <OAuth />
             </form>
           </div>
         </section>
