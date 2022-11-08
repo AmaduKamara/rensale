c28e3905f62b89c097a3c22f553e91c04a643581
Amkam <amaduskamara@gmail.com>
2022-11-08 09:44:23 +0000

    Add routes for pages with react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
-    <div>
+    <>
       <h1>Rentor</h1>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
 
