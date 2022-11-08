dd31fc97f9cc7cd4411341d85b8154a4185bc0c9
Amkam <amaduskamara@gmail.com>
2022-11-08 12:10:46 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const rentor = "./rentor-logo.png";
 
-const Header = ({ active, setActive }) => {
+const Header = () => {
+  const location = useLocation();
+
+  const pathRouteMatch = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
   return (
-    <header className="py-5 shadow-lg">
+    <header className="py-4 shadow-lg sticky top-0 w-screen z-50 bg-white">
       <div className="flex items-center justify-between container mx-auto">
         <Link to="/">
-          <img src={rentor} alt="Rentor Logo" className="h-6" />
+          <img src={rentor} alt="Rentor Logo" className="h-8" />
         </Link>
         <nav>
           <ul className="flex items-center space-x-10">
-            <li className="font-semibold text-gray-600 hover:text-cyan-500">
+            <li className="font-semibold text-gray-400 hover:text-cyan-500">
               <Link
-                className={`${
-                  active === "home" ? "text-cyan-500 underline" : ""
+                className={`border-b-[3px] py-5 border-transparent ${
+                  pathRouteMatch("/") && "text-cyan-500 border-b-cyan-500"
                 }`}
                 to="/"
-                onClick={() => setActive("home")}
               >
                 Home
               </Link>
             </li>
-            <li className="font-semibold text-gray-600 hover:text-cyan-500">
+            <li className="font-semibold text-gray-400 hover:text-cyan-500">
               <Link
-                className={`${
-                  active === "offers" ? "text-cyan-500 underline" : ""
+                className={`border-b-[3px] py-5 border-transparent ${
+                  pathRouteMatch("/offers") && "text-cyan-500 border-b-cyan-500"
                 }`}
                 to="/offers"
-                onClick={() => setActive("offers")}
               >
                 Offers
               </Link>
             </li>
-            <li className="font-semibold text-gray-600 hover:text-cyan-500">
+            <li className="font-semibold text-gray-400 hover:text-cyan-500">
               <Link
-                className={`${
-                  active === "signin" ? "text-cyan-500 underline" : ""
+                className={`border-b-[3px] py-5 border-transparent ${
+                  pathRouteMatch("/signin") && "text-cyan-500 border-b-cyan-500"
                 }`}
                 to="/signin"
-                onClick={() => setActive("signin")}
               >
                 Sign In
               </Link>
