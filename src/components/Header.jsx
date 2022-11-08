64b48f823708928d0ee4ec50ac0ca96c4676be58
Amkam <amaduskamara@gmail.com>
2022-11-08 11:08:27 +0000

    Add Signin link on header and spacing between links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ const rentor = "./rentor-logo.png";
 
 const Header = ({ active, setActive }) => {
   return (
-    <header className="py-4 shadow-lg">
+    <header className="py-5 shadow-lg">
       <div className="flex items-center justify-between container mx-auto">
         <Link to="/">
-          <img src={rentor} alt="Rentor Logo" width={120} />
+          <img src={rentor} alt="Rentor Logo" className="h-6" />
         </Link>
         <nav>
-          <ul className="flex items-center">
-            <li className="mx-5 font-semibold text-gray-600 hover:text-cyan-500">
+          <ul className="flex items-center space-x-10">
+            <li className="font-semibold text-gray-600 hover:text-cyan-500">
               <Link
                 className={`${
                   active === "home" ? "text-cyan-500 underline" : ""
@@ -23,7 +23,7 @@ const Header = ({ active, setActive }) => {
                 Home
               </Link>
             </li>
-            <li className="ml-5 font-semibold text-gray-600 hover:text-cyan-500">
+            <li className="font-semibold text-gray-600 hover:text-cyan-500">
               <Link
                 className={`${
                   active === "offers" ? "text-cyan-500 underline" : ""
@@ -32,6 +32,17 @@ const Header = ({ active, setActive }) => {
                 onClick={() => setActive("offers")}
               >
                 Offers
+              </Link>
+            </li>
+            <li className="font-semibold text-gray-600 hover:text-cyan-500">
+              <Link
+                className={`${
+                  active === "signin" ? "text-cyan-500 underline" : ""
+                }`}
+                to="/signin"
+                onClick={() => setActive("signin")}
+              >
+                Sign In
               </Link>
             </li>
           </ul>
