bda57acacaaca4452d4bf90123d3b49692606e33
Amkam <amaduskamara@gmail.com>
2022-11-09 03:38:07 +0000

    Add signin with google

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../firebase";
+import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
 
 const OAuth = () => {
+  const navigate = useNavigate();
+  const onGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Check for the user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  };
+
   return (
     <div className="flex items-center justify-center mt-2">
       <div className="h-12 w-12 rounded-full shadow-xl flex items-center justify-center border border-gray-100">
-        <Link to="/">
+        <button type="button" onClick={onGoogleClick}>
           <FcGoogle size={30} />
-        </Link>
+        </button>
       </div>
     </div>
   );
-}
+};
 
-export default OAuth
+export default OAuth;
